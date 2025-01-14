fdf9d9f4260a5422f210670f293d9df4f8643ca7
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2025-01-14 20:35:06 +0100

    si

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Partidas from './componentes/Partidas.jsx';
-
+import './App.css';  
+import Inicio from './componentes/inicio.jsx'
 
 const App = () => {
   return (
     <>
-      <h1>Bienvenido a tetris</h1>
+      <h1>Bienvenido a Tetris</h1>
       <Partidas />
+      <Inicio />
+
+      
     </>
   );
 };
